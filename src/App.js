48e5b1ee78d4b1037fb4a2ca3ff30debb89205aa
shivang <shivangmathur101@gmail.com>
2022-03-05 22:11:13 +0530

    bad way to update state

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [name, setName] = useState("Shivang");
   const [event, setEvent] = useState([
     { title: "Shivang's birthday party", id: 1 },
     { title: "date with Neha", id: 2 },
     { title: "Movie with friends", id: 3 },
   ]);
-  const handleClick = () => {
-    setName("Mathur");
-    console.log(name);
+  const handleClick = (id) => {
+    setEvent(
+      event.filter((el) => {
+        return el.id !== id;
+      })
+    );
   };
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change Name</button>
-
       {event.map((el, index) => (
         <div key={el.id}>
           <h2>
             {index + 1}) {el.title}
           </h2>
+          <button
+            onClick={() => {
+              handleClick(el.id);
+            }}
+          >
+            Delete Event
+          </button>
         </div>
       ))}
     </div>
