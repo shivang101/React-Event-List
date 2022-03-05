6262c70d8b9a034c04b869463755cb7a4fa2c1c6
shivang <shivangmathur101@gmail.com>
2022-03-05 22:29:39 +0530

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [showEvents, setShowEvents] = useState(true);
+
   const [event, setEvent] = useState([
     { title: "Shivang's birthday party", id: 1 },
     { title: "date with Neha", id: 2 },
@@ -21,20 +23,40 @@ function App() {
 
   return (
     <div className="App">
-      {event.map((el, index) => (
-        <div key={el.id}>
-          <h2>
-            {index + 1}) {el.title}
-          </h2>
-          <button
-            onClick={() => {
-              handleClick(el.id);
-            }}
-          >
-            Delete Event
-          </button>
-        </div>
-      ))}
+      <div>
+        <button
+          onClick={() => {
+            setShowEvents(false);
+          }}
+        >
+          <h2>Hide Events</h2>
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            setShowEvents(true);
+          }}
+        >
+          <h2>Show Events</h2>
+        </button>
+      </div>
+
+      {showEvents &&
+        event.map((el, index) => (
+          <div key={el.id}>
+            <h2>
+              {index + 1}) {el.title}
+            </h2>
+            <button
+              onClick={() => {
+                handleClick(el.id);
+              }}
+            >
+              Delete Event
+            </button>
+          </div>
+        ))}
     </div>
   );
 }
