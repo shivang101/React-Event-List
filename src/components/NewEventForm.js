b54c488f7dfad1e6aaecbcb57741efcdd8df48c2
shivang <shivangmathur101@gmail.com>
2022-03-08 19:54:46 +0530

    controlled input without useRef

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import "./NewEventForm.css";
 
-import React from "react";
-
 export default function NewEventForm() {
+  const [title, setTitle] = useState("");
+  const [date, setDate] = useState("");
+
+  const resetForm = () => {
+    setTitle("");
+    setDate("");
+  };
+
   return (
     <div>
       <form className="new-event-form">
@@ -10,13 +17,24 @@ export default function NewEventForm() {
         <input type="text" id="title" /> */}
         <label>
           <span>Event Title </span>
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
         </label>
         <label>
           <span> Event Date</span>
-          <input type="date" />
+          <input
+            type="date"
+            onChange={(e) => setDate(e.target.value)}
+            value={date}
+          />
         </label>
         <button> Submit</button>
+        <p>Title - {title}</p>
+        <p>Date - {date}</p>
+        <p onClick={resetForm}>Reset The Form</p>
       </form>
     </div>
   );
